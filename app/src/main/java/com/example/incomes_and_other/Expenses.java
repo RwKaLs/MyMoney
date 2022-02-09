889a7a88845981871d6d08c4d936c227313f85bf
Sparky324 <elaphantik@mail.ru>
2022-02-09 22:22:08 +0300

    1) Fragments for Incomes.java and Expenses.java ready. 2) Interface for their activities pre-ready. 3) The logic of switching Fragments is ready

--- app/src/main/java/com/example/incomes_and_other/Expenses.java
+++ app/src/main/java/com/example/incomes_and_other/Expenses.java
@@ -3,19 +3,48 @@ package com.example.incomes_and_other;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class Expenses extends AppCompatActivity {
+
+    //FrameLayout frameLayout = (FrameLayout)findViewById(R.id.spisok);
+    //private Fragment SpisokFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expenses);
+
+        //frameLayout.addView(SpisokFragment);
     }
 
     public void onBackClc2(View view){
         Intent intent = new Intent(Expenses.this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void onDiagClc(View view) {
+        DiagrammFragment diagrammFragment = new DiagrammFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.Cont_Exp, diagrammFragment);
+        ft.commit();
+    }
+
+    public void onImprtClc(View view) {
+        ImportFragment importFragment = new ImportFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.Cont_Exp, importFragment);
+        ft.commit();
+    }
+
+    public void onListClc(View view) {
+        SpisokFragment spisokFragment = new SpisokFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.Cont_Exp, spisokFragment);
+        ft.commit();
     }
 }
--- app/src/main/java/com/example/incomes_and_other/Incomes.java
+++ app/src/main/java/com/example/incomes_and_other/Incomes.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 public class Incomes extends AppCompatActivity {
 
@@ -18,5 +19,25 @@ public class Incomes extends AppCompatActivity {
         Intent intent = new Intent(Incomes.this, MainActivity.class);
         startActivity(intent);
     }
+
+    public void onListClc(View view) {
+        SpisokFragment2 spisokFragment2 = new SpisokFragment2();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.Cont_Inc, spisokFragment2);
+        ft.commit();
+    }
+
+    public void onImportClc(View view) {
+        ImportFragment2 importFragment2 = new ImportFragment2();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.Cont_Inc, importFragment2);
+        ft.commit();
+    }
+
+    public void onDiagClc(View view) {
+        DiagrammFragment2 diagrammFragment2 = new DiagrammFragment2();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.Cont_Inc, diagrammFragment2);
+        ft.commit();
+    }
 }
-// lazylist
