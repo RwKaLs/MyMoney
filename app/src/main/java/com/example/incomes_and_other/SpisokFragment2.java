0d903e282156163180c42dd31111d23bf276f440
Sparky324 <elaphantik@mail.ru>
2022-02-11 22:59:03 +0300

    1) Created adapter fo RecyclerView 2) Problem in SpisokFragment2

--- app/src/main/java/com/example/incomes_and_other/SpisokFragment2.java
+++ app/src/main/java/com/example/incomes_and_other/SpisokFragment2.java
@@ -3,16 +3,31 @@ package com.example.incomes_and_other;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
+
 public class SpisokFragment2 extends Fragment {
+
+    List<Income> incomes = new List<Income>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        // начальная инициализация списка
+        setInitialData();
+        RecyclerView recyclerView = getView().findViewById(R.id.list_inc);
+        // создаем адаптер
+        IncomeAdapter adapter = new IncomeAdapter(this, incomes);
+        // устанавливаем для списка адаптер
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -20,5 +35,12 @@ public class SpisokFragment2 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_spisok2, container, false);
+    }
+
+    private void setInitialData(){
+
+        incomes.add(new Income ("12.05.1999", 20000, "ЖКХ"));
+        incomes.add(new Income ("24.02.2006", 1500000, "Другое"));
+        incomes.add(new Income ("22.05.2014", 120, "Продукты"));
     }
 }
