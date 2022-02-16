62d2feda9f2e097e8f72f0cb47d2148477e19c8c
Sparky324 <elaphantik@mail.ru>
2022-02-16 22:22:05 +0300

    1) Added RecyclerView in fragment_spisok.xml 2) RecyclerView doesn't work)

---
+++ app/src/main/java/com/example/incomes_and_other/ListAdapter.java
@@ -0,0 +1,54 @@
+package com.example.incomes_and_other;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+
+    private final LayoutInflater inflater;
+    private ArrayList<Income> incomes;
+
+    ListAdapter(Context context, ArrayList<Income> incomes) {
+        this.incomes = incomes;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public ListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = inflater.inflate(R.layout.list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ListAdapter.ViewHolder holder, int position) {
+        Income income = incomes.get(position);
+        holder.summaView.setText(income.getSumma());
+        holder.typeView.setText(income.getType());
+        holder.dataView.setText(income.getData());
+    }
+
+    @Override
+    public int getItemCount() {     // TODO: разобраться, как подавать размер
+        return 4;   //incomes.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView typeView, dataView, summaView;
+        ViewHolder(View view){
+            super(view);
+            summaView = view.findViewById(R.id.SumListItem);
+            typeView = view.findViewById(R.id.TypeListItem);
+            dataView = view.findViewById(R.id.DateListItem);
+        }
+    }
+
+}
--- app/src/main/java/com/example/incomes_and_other/SpisokFragment.java
+++ app/src/main/java/com/example/incomes_and_other/SpisokFragment.java
@@ -3,13 +3,19 @@ package com.example.incomes_and_other;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SpisokFragment extends Fragment {
 
+    ArrayList<Income> incomes = new ArrayList<Income>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,26 @@ public class SpisokFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_spisok, container, false);
+
+        // начальная инициализация списка
+        setInitialData();
+        RecyclerView recyclerView = view.findViewById(R.id.list_Exp);
+        // создаем адаптер
+        ListAdapter adapter = new ListAdapter(this.getContext(), incomes);
+        // устанавливаем для списка адаптер
+        recyclerView.setAdapter(adapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spisok, container, false);
+        return view;
+    }
+
+    private void setInitialData(){
+        // Инициализация данных вручную для проверки
+        incomes.add(new Income ("12.05.1999", 100, "Другое"));
+        incomes.add(new Income ("24.02.2006", 1000, "ЖКХ"));
+        incomes.add(new Income ("01.01.2000", 550, "Другое"));
+        incomes.add(new Income ("07.10.2020", 25000, "Продукты"));
     }
 }
