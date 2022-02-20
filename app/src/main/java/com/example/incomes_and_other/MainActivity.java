0569311edafaf3536a6e90da9643d2a085fb6f57
RwKaLs <53008072+RwKaLs@users.noreply.github.com>
2022-02-20 19:01:12 +0300

    PREFINAL COMMIT

--- app/src/main/java/com/example/incomes_and_other/MainActivity.java
+++ app/src/main/java/com/example/incomes_and_other/MainActivity.java
@@ -30,7 +30,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.Date;
-
+//
 public class MainActivity extends AppCompatActivity {
 
     Button toInc, toExp, saveInc, saveExp;
